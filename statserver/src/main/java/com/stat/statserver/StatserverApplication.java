1c562e6e7fe9b4d0ac3b702dfc28674a6540b637
romanbizin <roman.bizin1995@gmail.com>
2023-11-03 23:46:28 +0300

    feat: add modules

---
+++ statserver/src/main/java/com/stat/statserver/StatserverApplication.java
@@ -0,0 +1,13 @@
+package com.stat.statserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatserverApplication.class, args);
+    }
+
+}
